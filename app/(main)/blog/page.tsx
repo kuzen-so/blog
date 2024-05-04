c052fd51ebd3c7d4fb65136235fee16df16595ad
Kuzen <xiaokundaa@gmail.com>
2024-05-04 23:50:48 +0800

    Update page.tsx

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -6,7 +6,8 @@ import { Container } from '~/components/ui/Container'
 import { BlogPosts } from './BlogPosts'
 
 const description =
-  '写博客文章是我比较喜欢的沉淀分享方式，我希望能够把好用的技术知识传递给更多的人。我比较喜欢围绕着技术为主的话题，但是也会写一些非技术的话题，比如设计、创业、企业管理、生活随笔等等。'
+  '写博客文章可以让我不追求点赞率的沉淀思考,我希望通过我的观点输出认识到一些志同道合的人。现在的我还是比较追求技术,所以喜欢围绕着技术为主的话题，但是也会写一些非技术的话题，比如设计、绘画、感悟、生活随笔等等.'
+  
 export const metadata = {
   title: '我的博客',
   description,
@@ -27,7 +28,7 @@ export default function BlogPage() {
     <Container className="mt-16 sm:mt-24">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          欢迎光临我的博客
+          欢迎您光临我的博客
         </h1>
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
