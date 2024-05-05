a4e6196c8441077f27088611a99e3177ca825d2a
Kuzen <xiaokundaa@gmail.com>
2024-05-05 09:18:05 +0800

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、设计师、细节控、创始人',
+  title: 'Kuzen • Scholar | 开发者、设计师',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    '我叫 Kuzen，一名开发者，设计师，细节控,我对开发和设计怀有热爱🔥，喜欢创新和探索新事物，期盼与大家有深度的交流📮',
   url: new URL(
     process.env.NODE_ENV === 'production'
       ? 'https://cali.so'
