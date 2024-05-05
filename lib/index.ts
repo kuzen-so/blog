9b9c1e620b3164ec1eb1e90b2f3355dc515b512c
Kuzen <xiaokundaa@gmail.com>
2024-05-05 18:26:16 +0800

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import { env } from '~/env.mjs'
 export function url(path = '') {
   const baseUrl =
     process.env.NODE_ENV === 'production'
-        'https://www.kuzen.top'
+      ? env.NEXT_PUBLIC_SITE_URL
       : 'http://localhost:3000'
 
   return new URL(path, baseUrl)
