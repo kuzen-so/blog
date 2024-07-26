c624dc7fcba547568740bdc3172dc0e470c47684
kuzen <xiaokundaa@gmail.com>
2024-07-26 12:29:50 +0800

    chore: 修复bug

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -117,11 +117,11 @@ export async function Footer() {
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
                 &copy; {new Date().getFullYear()} • Kuzen 
-                {/* • */}
-                {/* <strong>Fork 自</strong> */}
-                {/* <PeekabooLink href="https://github.com/CaliCastle/cali.so">
+                •
+                <strong>Fork 自</strong>
+                <PeekabooLink href="https://github.com/CaliCastle/cali.so">
                   cali.so
-                </PeekabooLink> */}
+                </PeekabooLink>
               </p>
               <Links />
             </div>
