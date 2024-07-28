4076c8f22a4b37ece9a3ab5a35aebfc3cd1b280b
kuzen <xiaokundaa@gmail.com>
2024-07-29 05:49:33 +0800

    chore: 修复一些bug

--- app/blocked/page.tsx
+++ app/blocked/page.tsx
@@ -7,7 +7,7 @@ export default function BlockedPage() {
       <span className="text-sm">
         如果你认为你不应该被禁，请联系我的邮箱{' '}
         <a href="mailto:xiaokundaa@gmail.com" className="font-bold underline">
-          hi@cali.so
+          hi@kuzen.top
         </a>
       </span>
     </main>
