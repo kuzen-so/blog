03d5f3b1ba9bb7cbbc8285efdcf433a2e93a58eb
kuzen <xiaokundaa@gmail.com>
2024-07-29 05:47:18 +0800

    chore: 修复一些bug

--- lib/seo.ts
+++ lib/seo.ts
@@ -4,7 +4,7 @@ export const seo = {
     '我叫 Kuzen，一名开发者，设计师，细节控,我对开发和设计怀有热爱🔥，喜欢创新和探索新事物，期盼与大家有深度的交流📮',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://www.kuzen.top/'
       : 'http://localhost:3000'
   ),
 } as const
